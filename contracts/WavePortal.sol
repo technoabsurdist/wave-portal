32adbc446b66fa295bae89bbd6435c07e93ac33f
Emi Andere <andere@cs.uchicago.edu>
2022-02-27 20:04:35 -0600

    first commit

---
+++ contracts/WavePortal.sol
@@ -0,0 +1,28 @@
+// SPDX-License-Identifier: UNLICENSED
+
+pragma solidity ^0.8.0; 
+
+import "hardhat/console.sol"; 
+
+contract WavePortal {
+    uint totalWaves; 
+
+    constructor() {
+        console.log("Yo yo, I am a contract and I am smart"); 
+    }
+
+    function wave() public {
+        totalWaves++; 
+        console.log("%s has waved", msg.sender); 
+    }
+
+    function getTotalWaves() public view returns (uint) {
+        console.log("We have %d total waves!", totalWaves);
+        return totalWaves; 
+    }
+}
+
+
+
+
+
